0a71c0c9ab403e4abf07dc1487e2bc1f2f725b32
Durgesh094 <durgeshchavan1994@gmail.com>
2022-06-06 08:55:59 +0530

    Three variables initialise with random two digit number

--- Arithmatic_Sorting.sh
+++ Arithmatic_Sorting.sh
@@ -1,9 +1,15 @@
 
 echo "Welcome to Arithmatic Computation and Sorting Program";
 
-read -p "Enter the value of a: " a; 
-read -p "Enter the value of b: " b; 
-read -p "Enter the value of c: " c;
+#read -p "Enter the value of a: " a; 
+#read -p "Enter the value of b: " b; 
+#read -p "Enter the value of c: " c;
+
+a=$((RANDOM%90+10));
+b=$((RANDOM%90+10));
+c=$((RANDOM%90+10));
+
+echo $a $b $c;
 
 expr[0]=$(($a+$b*$c)); #UC2
 expr[1]=$(($a*$b+$c)); #UC3
