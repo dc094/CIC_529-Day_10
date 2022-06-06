5c122cf2a3dc5d3d9b447b5fecb35adcad2f93fc
Durgesh094 <durgeshchavan1994@gmail.com>
2022-06-06 08:40:04 +0530

    a+b*c computation

--- Arithmatic_Sorting.sh
+++ Arithmatic_Sorting.sh
@@ -4,3 +4,7 @@ echo "Welcome to Arithmatic Computation and Sorting Program";
 read -p "Enter the value of a: " a; 
 read -p "Enter the value of b: " b; 
 read -p "Enter the value of c: " c;
+
+expr[0]=$(($a+$b*$c));
+
+echo ${expr[0]};
