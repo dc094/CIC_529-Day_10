468b3cd775231ca0845bcb539dacdd57c62ef683
Durgesh094 <durgeshchavan1994@gmail.com>
2022-06-06 08:48:12 +0530

    Computations stored with an array

--- Arithmatic_Sorting.sh
+++ Arithmatic_Sorting.sh
@@ -5,6 +5,9 @@ read -p "Enter the value of a: " a;
 read -p "Enter the value of b: " b; 
 read -p "Enter the value of c: " c;
 
-expr[0]=$(($a+$b*$c));
+expr[0]=$(($a+$b*$c)); #UC2
+expr[1]=$(($a*$b+$c)); #UC3
+expr[2]=$(($c+$a/$b)); #UC4
+expr[3]=$(($a%$b+$c)); #UC5
 
-echo ${expr[0]};
+echo ${expr[@]};
